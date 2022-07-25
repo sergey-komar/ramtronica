0a7ff98e80c7917e783d1269701423ff38da9bea
sergey-komar <kursev81@yandex.ru>
2022-07-25 23:26:37 +0300

    heder

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
-
+    $('.slider__inner').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev-btn"></button>',
+        nextArrow: false
+    })
 })
